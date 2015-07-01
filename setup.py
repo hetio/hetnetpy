3c06dfd04b7042ed26f7f4853d285346af00bc8b
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2015-07-01 14:00:47 -0700

    Added install script

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name = 'hetio',
+    version = '0.1.0',
+    author = 'Daniel Himmelstein',
+    author_email = 'daniel.himmelstein@gmail.com',
+    url = 'https://github.com/dhimmel/hetio',
+    description = 'Heterogeneous networks',
+    license = 'CC0',
+    packages = ['hetio'],
+    )
