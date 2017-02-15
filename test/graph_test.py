69aea626c0de331f78ad2538951742e5b2cffc0a
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-02-15 17:17:16 -0500

    Fix tests with updated import path

--- test/graph_test.py
+++ test/graph_test.py
@@ -2,6 +2,7 @@ import pytest
 
 import hetio.hetnet
 
+
 def test_creation():
     metaedge_tuples = [
         ('compound', 'disease', 'indication', 'both'),
@@ -9,12 +10,12 @@ def test_creation():
         ('compound', 'gene', 'target', 'both'),
     ]
     metanode_ids = 'compound', 'disease', 'gene'
-    metagraph = graph.MetaGraph.from_edge_tuples(metaedge_tuples)
+    metagraph = hetio.hetnet.MetaGraph.from_edge_tuples(metaedge_tuples)
 
     # check that nodes got added to metagraph_node_dict
     assert frozenset(metagraph.node_dict) == frozenset(metanode_ids)
     for metanode in metagraph.node_dict.values():
-        assert isinstance(metanode, graph.MetaNode)
+        assert isinstance(metanode, hetio.hetnet.MetaNode)
 
     # check that metanode.get_id() and hash(metanode) are working as expected
     for metanode_id in metanode_ids:
@@ -23,12 +24,12 @@ def test_creation():
         assert metanode.get_id() == metanode_id
         assert hash(metanode) == hash(metanode_id)
 
-    g = graph.Graph(metagraph)
-    ms = g.add_node('disease', 'DOID:2377', 'multiple sclerosis')
+    graph = hetio.hetnet.Graph(metagraph)
+    ms = graph.add_node('disease', 'DOID:2377', 'multiple sclerosis')
     assert ms.metanode.identifier == 'disease'
     assert ms.identifier == 'DOID:2377'
     assert ms.name == 'multiple sclerosis'
 
     with pytest.raises(KeyError):
         # misordered args
-        g.add_node('DOID:2377', 'multiple sclerosis', 'disease')
+        graph.add_node('DOID:2377', 'multiple sclerosis', 'disease')
