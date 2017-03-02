a34946f18a6ba4ca2eafd5acadc30553b9fcb38c
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-03-02 18:29:01 -0500

    Remove register.py, switching to Travis deploy

--- register.py
+++
@@ -1,9 +0,0 @@
-import os
-import pypandoc
-
-long_description = pypandoc.convert('README.md', 'rst', outputfile='README.rst')
-
-os.system("python setup.py register -r pypi")
-os.system("python setup.py sdist upload -r pypi")
-
-os.remove('README.rst')
