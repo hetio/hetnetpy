432f14b59be2bb0c0e230e44e4e02cd687c680fc
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-03-16 13:45:39 -0400

    Reformat setup.py. Configure extras_require

--- setup.py
+++ setup.py
@@ -3,29 +3,35 @@ import subprocess
 import setuptools
 
 
-# Try to create an rst long_description from README.md
 try:
+    # Try to create an reStructuredText long_description from README.md
     args = 'pandoc', '--from', 'markdown', '--to', 'rst', 'README.md'
     long_description = subprocess.check_output(args)
     long_description = long_description.decode()
 except Exception as error:
+    # Fallback to markdown (unformatted on PyPI) long_description
     print('README.md conversion to reStructuredText failed. Error:')
     print(error)
-    print('Setting long_description to None.')
-    long_description = None
+    with open('README.md') as read_file:
+        long_description = read_file.read()
 
 
 setuptools.setup(
-    name = 'hetio',
-    version = '0.2.3',
-    author = 'Daniel Himmelstein',
-    author_email = 'daniel.himmelstein@gmail.com',
-    url = 'https://github.com/dhimmel/hetio',
-    description = 'Hetnets in Python',
+    # Package details
+    name='hetio',
+    version='0.2.3',
+    url='https://github.com/dhimmel/hetio',
+    description='Hetnets in Python',
     long_description=long_description,
-    license = 'CC0 1.0',
-    packages = ['hetio'],
-    classifiers = [
+    license='CC0 1.0',
+
+    # Author details
+    author='Daniel Himmelstein',
+    author_email='daniel.himmelstein@gmail.com',
+
+    # Package topics
+    keywords='hetnet graph heterogeneous network neo4j hetio',
+    classifiers=[
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         'Topic :: Scientific/Engineering :: Information Analysis',
@@ -37,7 +43,16 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
     ],
 
-    install_requires = [
-        'tqdm'
-    ]
+    packages=['hetio'],
+
+    # Run-time dependencies
+    install_requires=[
+        'regex',
+    ],
+
+    # Additional groups of dependencies
+    extras_require={
+        'stats': ['pandas', 'matplotlib', 'seaborn'],
+        'neo4j': ['pandas', 'py2neo', 'tqdm'],
+    }
 )
