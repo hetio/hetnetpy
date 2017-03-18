488d603dbec663b38a95ba3315d83ae4bb4a5f21
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-03-18 17:06:18 -0400

    Add hetio.__version__ support
    See https://packaging.python.org/single_source_version/

--- setup.py
+++ setup.py
@@ -1,25 +1,38 @@
+import os
+import re
 import subprocess
 
 import setuptools
 
 
+directory = os.path.dirname(os.path.abspath(__file__))
+
+# version
+init_path = os.path.join(directory, 'hetio', '__init__.py')
+with open(init_path) as read_file:
+    text = read_file.read()
+pattern = re.compile(r"^__version__ = ['\"]([^'\"]*)['\"]", re.MULTILINE)
+version = pattern.search(text).group(1)
+
+# long_description
+readme_path = os.path.join(directory, 'README.md')
 try:
     # Try to create an reStructuredText long_description from README.md
-    args = 'pandoc', '--from', 'markdown', '--to', 'rst', 'README.md'
+    args = 'pandoc', '--from', 'markdown', '--to', 'rst', readme_path
     long_description = subprocess.check_output(args)
     long_description = long_description.decode()
 except Exception as error:
     # Fallback to markdown (unformatted on PyPI) long_description
     print('README.md conversion to reStructuredText failed. Error:')
     print(error)
-    with open('README.md') as read_file:
+    with open(readme_path) as read_file:
         long_description = read_file.read()
 
 
 setuptools.setup(
     # Package details
     name='hetio',
-    version='0.2.4',
+    version=version,
     url='https://github.com/dhimmel/hetio',
     description='Hetnets in Python',
     long_description=long_description,
