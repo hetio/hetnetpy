52a41c70fc9f46ad4b501510bd228692b6f47a4e
Michael Zietz <michael.zietz@gmail.com>
2018-12-05 13:52:51 -0500

    permute_pair_list: default to inplace=False
    Closes https://github.com/hetio/hetio/issues/28
    
    Adds test for permute_pair_list.
    Fixes unrelated filename typo.

---
+++ test/permute_test.py
@@ -0,0 +1,18 @@
+import pytest
+
+import hetio.permute
+
+
+@pytest.mark.parametrize('edges,inplace', [
+    ([(0, 0), (1, 1), (1, 2), (2, 3)], True),
+    ([(0, 0), (1, 1), (1, 2), (2, 3)], False),
+])
+def test_permute_inplace(edges, inplace):
+    old_edges = edges.copy()
+    new_edges, stats = hetio.permute.permute_pair_list(edges, inplace=inplace)
+    assert old_edges != new_edges
+
+    if inplace:
+        assert edges == new_edges
+    else:
+        assert edges != new_edges
