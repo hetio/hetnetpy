401f3a550d74c55ecc91065223013426288c7f99
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2019-07-10 15:58:16 -0400

    attempt fix
    https://stackoverflow.com/questions/22661745/why-only-one-warning-in-a-loop

--- test/relocation_test.py
+++ test/relocation_test.py
@@ -7,6 +7,6 @@ def test_relocation_warning():
     """
     https://github.com/hetio/hetio/issues/40
     """
-    warnings.simplefilter('always')
+    warnings.simplefilter('always', FutureWarning)
     with pytest.warns(FutureWarning, match='PACKAGE HAS BEEN RELOCATED'):
         import hetio.hetnet  # noqa F401
